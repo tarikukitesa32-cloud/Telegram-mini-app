420d530ba891b5ae366e42f1375a28bb215969a4
tarikukitesa32-cloud <tarikukitesa32@gmail.com>
2026-07-26 12:34:22 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,47 @@
+// Admin Login
 
+function adminLogin() {
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const message = document.getElementById("message");
+
+    const adminUser = "admin";
+    const adminPass = "123456";
+
+    if (username === adminUser && password === adminPass) {
+
+        localStorage.setItem("adminLoggedIn", "true");
+
+        window.location.href = "admin.html";
+
+    } else {
+
+        message.innerHTML = "❌ Invalid Username or Password";
+        message.style.color = "red";
+
+    }
+
+}
+
+
+// Check Login
+function checkAdmin(){
+
+    if(localStorage.getItem("adminLoggedIn") !== "true"){
+
+        window.location.href = "admin-login.html";
+
+    }
+
+}
+
+
+// Logout
+function logout(){
+
+    localStorage.removeItem("adminLoggedIn");
+
+    window.location.href = "admin-login.html";
+
+}
